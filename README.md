13a35100ad9f76c22f336b977ab53ff80ef1f41e
XeQtter <obeogo@yahoo.com>
2022-12-27 20:00:22 -0700

    Added new file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CodeGenerator
 # Coming soon
+This is a code generator that prints 30 five-letter string. The goal is to make those strings means something in English but for now this what I have.
--- codegenerator.py
+++ codegenerator.py
@@ -1,1 +1,10 @@
+# This is my first professional project that will be used for work purpose!
+
+import random
+import string
+
+for i in range(30):
+    # get random string of length 5 without repeating letters
+    result_str = ''.join(random.sample(string.ascii_lowercase, 5))
+    print(result_str)
 
